b955330ad5005c073dd17b50105d931353247c01
Austin Booth <austinbooth78@gmail.com>
2023-06-23 00:11:22 +0100

    Allow styling of Mathfield (class was not working, so changed to style)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react"
 import Mathfield from "./Mathfield"
 function App() {
-  const [value, setValue] = useState("x^2 + y^2 = z^2")
+  const [value, setValue] = useState('')
 
   return (
     <>
       <div>
         <h1>Mathfield</h1>
-        <Mathfield value={value} onChange={setValue} />
+        <Mathfield value={value} onChange={setValue} style={{width: '100px', padding: '4px 5px'}} />
         <div>
           <h2>Output</h2>
           <pre>{value}</pre>
--- src/Mathfield.tsx
+++ src/Mathfield.tsx
@@ -1,17 +1,17 @@
-import { useEffect, useRef, FC, useCallback } from 'react'
+import { useEffect, useRef, FC, useCallback, CSSProperties } from 'react'
 import { MathfieldElement, MathfieldOptions } from 'mathlive'
 
 export type MathfieldProps = {
   // options?: Partial<MathfieldOptions>;
   value: string;
   onChange: (latex: string) => void;
-  className?: string;
+  style?: CSSProperties;
 }
 
 // The math-field does not work if the next line is commented out.
 new MathfieldElement()
 
-const MathField: FC<MathfieldProps> = ({ value, onChange, className }) => {
+const MathField: FC<MathfieldProps> = ({ value, onChange, style }) => {
   const mfRef = useRef<MathfieldElement>(null)
 
   const handleInputChange = useCallback(() => onChange(mfRef.current?.value ?? ''), [onChange])
@@ -34,7 +34,7 @@ const MathField: FC<MathfieldProps> = ({ value, onChange, className }) => {
   }, [])
 
   return (
-    <math-field ref={mfRef} class={className}>{value}</math-field>
+    <math-field ref={mfRef} style={style}>{value}</math-field>
   )
 }
 
